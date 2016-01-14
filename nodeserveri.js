d21ea70140fb17564e16c8f09255582dd6e104d0
kebabmestari <lindqvist.samuel@gmail.com>
2016-01-15 00:01:44 +0200

    Kakkone
    Sibale
    adssda

---
+++ nodeserveri.js
@@ -0,0 +1,25 @@
+//Ota http moduuli
+var http = require('http');
+
+console.log('Start');
+
+//Luo serveri
+var server = http.createServer(function(req, res) {
+	var posti = '';
+		res.writeHead(200); //OK
+		res.end('Lähetetty data: ' + posti);
+		res.send();
+	console.log('HTTP requesti tullut... kasitellaan');
+	req
+	.on('data', function(chunk) { //Jos tulee POST dataa
+		if( req.method == 'POST' ){
+			posti += chunk || 'EI DATAA';
+			console.log('Tullut data: ' + chunk);
+		}
+	})
+	.on('end', function() { //Response
+		res.writeHead(200); //OK
+		res.end('Lähetetty data: ' + posti);
+	});
+});
+server.listen(8080);
